7aa68724eefcb345423edeefb291b76afbfd0d25
James Taylor <54148103+jumbojets@users.noreply.github.com>
2024-03-24 11:59:07 -0400

    more hein

--- sdk/blindr_driver.py
+++ sdk/blindr_driver.py
@@ -1,14 +1,10 @@
 from blindr_config import BlindrConfig
 from blindr_client import MyAPIClient
-from libblindr import hash_spend_constraint
+
 
 my_config = BlindrConfig(base_url="http://127.0.0.1:5000", api_key="")
 my_client = MyAPIClient(my_config)
 
-# my_client.create_new_session("constraint1")
-# print(my_client.restart_session_with_hash("constraint1"))
-
-# print(my_client.sign_message("message1", "public_value1", "public_key1", "constraint1"))
 
 constraint = """{
 	"auth": [
@@ -20,21 +16,55 @@ constraint = """{
 }"""
 
 message = """{
-	"sender": "asdfasdf",
-	"receiver": "asdfasdf",
+	"sender": "1FWu4Z9NoBWnguurBCdXpmM2xuiog6kbdy",
+	"receiver": "3C3nZhpVjjDGo7vGzBCTJkKfYzCGWGLWsq",
+	"amount": 100
+}"""
+
+try:
+	signature = my_client.create_session_and_sign(message, constraint)
+	print(f"signature is {signature}")
+	if my_client.delete_key(constraint):
+		print("Deleted key")
+
+except:
+	if my_client.delete_key(constraint):
+		print("Deleted key")
+
+
+
+
+
+
+
+
+constraint = """{
+	"auth": [
+		["password", "james duke123"],
+		["What is your mothers maiden name", "Duke"],
+    ["What is your favorite ice cream flavor", "chocolate"]
+    ],
+    "withdrawal_lximit" : 10
+}"""
+
+message = """{
+	"sender": "1FWu4Z9NoBWnguurBCdXpmM2xuiog6kbdy",
+	"receiver": "3C3nZhpVjjDGo7vGzBCTJkKfYzCGWGLWsq",
 	"amount": 100
 }"""
 
 
 try:
-	current_session = my_client.create_new_session(constraint)
+	
+	current_session = my_client.create_or_restart_session(constraint)
 
 	# current_session = my_client.restart_session_with_hash(constraint)
 
 	signature = my_client.sign_message(message, current_session['public_value'], current_session['public_key'], constraint)
+	print(f"signature is {signature}")
 	if my_client.delete_key(constraint):
 		print("Deleted key")
-	
+
 except:
 	if my_client.delete_key(constraint):
 		print("Deleted key")
